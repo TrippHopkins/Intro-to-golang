fa592a75268420904aa25844f7a69a936aceafa6
TrippHopkins <tripphopkins13245@gmail.com>
2025-01-31 12:11:11 -0500

    Go lang startings

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+//declares what file this is. GoLang uses different
+//code for each package.
+//main is a special package which configures the exicutables
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello")
+}
