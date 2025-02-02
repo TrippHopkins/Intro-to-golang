bb54731dcfd9947a7999992827f6df4673d55a2e
TrippHopkins <tripphopkins13245@gmail.com>
2025-02-02 12:39:04 -0500

    Tested, added subtract

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
+
+import "fmt"
+
 //declares what file this is. GoLang uses different
 //code for each package.
 //main is a special package which configures the exicutables
 
-import "fmt"
+// "fmt" is a liberary that includes the "printing to terminal" functions
 
-func main(){
-	fmt.Println("Hello")
+func main() {
+
+	var Num1 float32 = 20.5
+	var Num2 float32 = .5
+	var subtractResult float32 = subtract(Num1, Num2)
+	fmt.Println(subtractResult)
+
+}
+func subtract(Num1 float32, Num2 float32) float32 {
+
+	var subtractionResult float32 = Num1 - Num2
+
+	return subtractionResult
 }
