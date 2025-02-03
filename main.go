c5447904cbcbf97515e02197fa5bfef892d61086
TrippHopkins <tripphopkins13245@gmail.com>
2025-02-03 11:07:55 -0500

    Added: divde, errors, if{}, switch{}

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 //declares what file this is. GoLang uses different
 //code for each package.
@@ -10,15 +13,37 @@ import "fmt"
 
 func main() {
 
-	var Num1 float32 = 20.5
-	var Num2 float32 = .5
-	var subtractResult float32 = subtract(Num1, Num2)
-	fmt.Println(subtractResult)
+	var Num1 int = 20
+	var Num2 int = 0
+	var result, remainder, err = divide(Num1, Num2)
+	switch {
+	case err != nil:
+		fmt.Printf(err.Error())
+	case remainder == 0:
+		fmt.Printf("The result is %v", result)
+	default:
+		fmt.Printf("The result is %v and the remainder is %v", result, remainder)
+
+	}
+	/*if err != nil {
+		fmt.Printf(err.Error())
+	} else if remainder == 0 {
+		fmt.Printf("The result is %v", result)
+	} else {
+		fmt.Printf("The result is %v and the remainder is %v", result, remainder)
+
+	}*/
 
 }
-func subtract(Num1 float32, Num2 float32) float32 {
 
-	var subtractionResult float32 = Num1 - Num2
+func divide(Num1 int, Num2 int) (int, int, error) {
+	var err error
+	if Num2 == 0 {
+		err = errors.New("Cannot Divide by Zero")
+		return 0, 0, err
+	}
+	var result int = Num1 / Num2
+	var remainder int = Num1 % Num2
 
-	return subtractionResult
+	return result, remainder, err
 }
